df01cb1a5a48ae58b0bdf04da2c54ee072f1aaa4
mick26 <mcullen26@gmail.com>
2014-11-11 10:12:01 +0000

    mobile improved

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,11 @@
+/*
+Ref.
+http://stackoverflow.com/questions/20718020/angularjs-ngtouch-300ms-delay
+*/
+
 
 'use strict';
 /*================================================
 Module - Main App Module
 ================================================ */
-angular.module('myApp', ['btford.socket-io' ,'myApp.services' ,'myApp.controllers'] );
+angular.module('myApp', ['btford.socket-io' ,'myApp.services' ,'myApp.controllers'] )
--- public/js/controllers/controllers.js
+++ public/js/controllers/controllers.js
@@ -1,6 +1,15 @@
 /*
 
 Ref.
+http://stackoverflow.com/questions/20718020/angularjs-ngtouch-300ms-delay
+https://www.youtube.com/watch?v=xOAG7Ab_Oz0
+https://github.com/ftlabs/fastclick
+
+http://jsfiddle.net/marcosecchi/DqWU2/10/
+http://jsfiddle.net/guillaumebiton/R8mmR/6/
+https://groups.google.com/forum/#!topic/angular/cx3rdqIKKx4
+http://jsfiddle.net/deshartman/A7VqN/10/
+
 https://www.mapbox.com/mapbox.js/example/v1.0.0/disable-world-wrapping/
 http://thunderforest.com/tutorials/leaflet/
 
@@ -12,8 +21,9 @@ x is the tile number from left to right,
 and y is the tile number from top to bottom. 
 For example, the solitary tile on zoom 0 is named 0/0/0.png
 
-
 */
+
+
 'use strict';
 
 /*================================================
@@ -25,37 +35,57 @@ angular.module('myApp.controllers', [])
 /*================================================
 Controller
 ================================================ */
-.controller('MainCtrl', function ($scope, $rootScope, $http, $location, socket) {
+.controller('MainCtrl', function ($scope, $interval, $timeout, socket) {
 
-	/*
-	 * create Date Object
-	 * Date.now() returns the number of milliseconds elapsed since 1 January 1970 00:00:00 UTC.
-	 */
+
+	/* ================================================
+	create Date Object
+	Date.now() returns the number of milliseconds elapsed since 1 January 1970 00:00:00 UTC.
+	================================================ */
 	$scope.date = new Date();	
 
-	/*
-	 * Declare variables
-	 */
+
+	/* ================================================
+	Declare Variables
+	================================================ */
 	var map;
 	var info = $scope.infobox="";
 	var info = $scope.infobox;
 	var sentData = {};
 	var connects = {}; //other users on map
 	var markers = {};
-	var active = false;
-	var emit;
+	var myLifetime = 30; //every 30sec need to send heartbeat to Node or will disappear from maps
+	var timerInterval = 1000; //1 second
+	var countdownTimer;
+	var myTimeout;
+
+	$scope.active = false;
 	$scope.errorMsg="";
+	$scope.eventCnt=0; //TEST
+	$scope.counter=0;
 
 
-	/*
-	 * Generate unique user id
-	 */
+	/*************************************************
+	 * Check if browser support HTML5 GeoLocation API
+	 ************************************************/
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(positionSuccess, positionError, { enableHighAccuracy: true });
+	} 
+	else {
+		$scope.errorMsg = 'Your browser does not support GeoLocation!';
+	}
+	/*************************************************/
+
+
+	/* ================================================
+	Generate unique user id
+	================================================ */
 	var userId = Math.random().toString(16).substring(2,15);
 
 
-	/*
-	 * custom marker's icon styles
-	 */
+	/* ================================================
+	Custom marker's icon styles
+	================================================ */
 	var tinyIcon = L.Icon.extend({
 		options: {
 			shadowUrl: '../assets/marker-shadow.png',
@@ -68,9 +98,9 @@ Controller
 	});
 
 
-	/*
-	 * Icons - yellow and red
-	 */
+	/* ================================================
+	Create new yellow and red Icon objects
+	================================================ */
 	var redIcon = new tinyIcon({ iconUrl: '../assets/marker-red.png' });  //new user
 	var yellowIcon = new tinyIcon({ iconUrl: '../assets/marker-yellow.png' }); 
 
@@ -96,9 +126,9 @@ Controller
 		 */
 		connects[data.id] = data; //Add user to object
 		
-		//console.log("connects = "+JSON.stringify(connects)); //TEST
 		/*
 		 * Add timestamp to data Object
+		 * Use the timestamp to determind when to remove inactive users from map
 		 * data ={"id":"48614f75b1fa2","active":true,"coords":[{"lat":50.2.,"lng":-7.2,"acr":0}],"updated":1415195059693}
 		 */
 		connects[data.id].updated = Date.now();
@@ -113,19 +143,10 @@ Controller
 	});
 
 
-	/*
-	 * check browser support for HTML5 GeoLocation API
-	 */
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(positionSuccess, positionError, { enableHighAccuracy: true });
-	} 
-	else {
-		$scope.errorMsg = 'Your browser does not support GeoLocation!';
-	}
 
 
 	/*********************************************
-	 * GeoLocation was got - successCallback 
+	 * GeoLocation was got from API - successCallback 
 	 *********************************************/
 	function positionSuccess(position) {
 		var lat = position.coords.latitude;
@@ -138,12 +159,6 @@ Controller
 		var userMarker = L.marker([lat, lng], {
 			icon: redIcon
 		});
-
-		/*
-		 * uncomment for testing
-		 * lets you show another marker on the map at a different location
-		 */
-		//userMarker = L.marker([51.4, 30.0], { icon: redIcon });
 
 
 		/*
@@ -166,38 +181,40 @@ Controller
 
 
 
-	    /*
-	     * This is another Map option that Works 
-	     * Adding a Thunderforest map layer
-	     */
-		/*
+		/* ======================================================================
+		This is another Map option that Works
+		Adding a Thunderforest map layer
+		=========================================================================
 	    var tileUrl = 'http://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png',
 	    layer = new L.TileLayer(tileUrl, {
 			    	maxZoom: 18,         
-			        // This option disables loading tiles outside of the world bounds.
+		 	        // This option disables loading tiles outside of the world bounds.
 			        noWrap: true
 			    }
 		);
-		*/
+		======================================================================= */
 
 
-		/*
-		 * variable to hold the date
-		 * needed to calculate 
-		 */
-		emit = Date.now();
 
 	  	/*
-		 * A user is active when the mouse is moving over the map area
-		 * If Active then send coords
+		 * Whenever either the 'Send location to Server' or 'Send Keep Alive Signal' 
+		 * buttons are pressed the user is flagges as active and its coordinates are send to
+		 * the Node server in a Socket.io packet
 		 */
-		$scope.mouseMoveAction = function() {
+		$scope.sendKeepAlive = function() {
 
-			active = true;
+			console.log("KeepAlive"); //TEST
 
+			//$scope.dataLastActive = Date.now();
+
+			$scope.eventCnt++; //TEST
+
+			/*
+			 * Build the sendData Obj with location details etc
+			 */
 			sentData = {
 				id: userId,
-				active: active,
+				active: $scope.active,
 				coords: [{
 					lat: lat,
 					lng: lng,
@@ -205,25 +222,16 @@ Controller
 				}]
 			};
 
+
 			/*
-			 * if more that30ms
+			 * Send sentData Obj to server
 			 */
-			if (Date.now() - emit > 30) {
-				socket.emit('send:coords', sentData);
-				emit = Date.now();
-			}
+			socket.emit('send:coords', sentData);
+		
+			$scope.refresh();
+			
 		};
-	}
-
-
-	/*
-	 * User Not Active on map 
-	 */
-
-	$scope.mouseLeaveAction=function() {
-		active = false;
 	};
-
 
 
 	/*
@@ -239,10 +247,10 @@ Controller
 
 
 
-	/*********************************************
+	/***************************************************************
 	 * GeoLocation was NOT got - errorCallback 
 	 * - handle geolocation api errors
-	 *********************************************/
+	 **************************************************************/
 	function positionError(error) {
 		var errors = {
 			1: 'Authorization fails', // permission denied
@@ -253,21 +261,57 @@ Controller
 		//Create the error message string that will be displayed
 		$scope.errorMsg = "Error:" + errors[error.code];
 	};
+	/***************************************************************/
 
 
 
+	$scope.counter = myLifetime-1;
 
-	/*
-	 * Delete inactive users every 15 sec
+	function setTimer () {
+		$scope.counter--;	
+		myTimeout = $timeout(setTimer,1000);
+		$scope.active = true;
+
+		if($scope.counter <= 0) {
+			$scope.cancelTimer()
+		}
+	};
+
+
+	$scope.cancelTimer = function() {
+    	$timeout.cancel(myTimeout);
+    	$scope.active=false;
+	}
+
+
+    $scope.refresh = function() {
+    	$timeout.cancel(myTimeout);
+    	$scope.counter = myLifetime;
+    	setTimer();
+    }
+
+
+
+	/* 
+	 * Delete inactive users every 30 sec
 	 */
 	setInterval(function() {
 		for (var ident in connects) {
 
-			if (Date.now() - connects[ident].updated > 15000) {
+			if (Date.now() - connects[ident].updated > myLifetime*1000) {
 				delete connects[ident];
 				map.removeLayer(markers[ident]);
 			}
 		}
 	}, 15000);
 
+
+
+	/* ================================================
+	Scope Destroy event
+	================================================ */
+	$scope.$on('$destroy',function() {
+    	if(intervalPromise)
+       		$interval.cancel(intervalPromise);   
+	});
 });
